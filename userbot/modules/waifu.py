9b94bf0880b3d5000e5b8c450274aff72e352474
AtomicXZ <achubadyal4@gmail.com>
2021-03-18 00:02:05 +0000

    waifu: Remove unnecessary delete timer * Minor cleanup
    Signed-off-by: AtomicXZ <achubadyal4@gmail.com>

--- userbot/modules/waifu.py
+++ userbot/modules/waifu.py
@@ -12,16 +12,16 @@ from userbot.events import register
 
 EMOJI_PATTERN = re.compile(
     "["
-    "\U0001F1E0-\U0001F1FF"  
-    "\U0001F300-\U0001F5FF"  
-    "\U0001F600-\U0001F64F"  
-    "\U0001F680-\U0001F6FF"  
-    "\U0001F700-\U0001F77F"  
-    "\U0001F780-\U0001F7FF"  
-    "\U0001F800-\U0001F8FF"  
-    "\U0001F900-\U0001F9FF"  
-    "\U0001FA00-\U0001FA6F"  
-    "\U0001FA70-\U0001FAFF"  
+    "\U0001F1E0-\U0001F1FF"
+    "\U0001F300-\U0001F5FF"
+    "\U0001F600-\U0001F64F"
+    "\U0001F680-\U0001F6FF"
+    "\U0001F700-\U0001F77F"
+    "\U0001F780-\U0001F7FF"
+    "\U0001F800-\U0001F8FF"
+    "\U0001F900-\U0001F9FF"
+    "\U0001FA00-\U0001FA6F"
+    "\U0001FA70-\U0001FAFF"
     "\U00002702-\U000027B0"
     "]+"
 )
@@ -55,7 +55,6 @@ async def waifu(cringe):
         return await cringe.edit(
             "**You cannot send inline results in this chat.**"
         )
-    await sleep(5)
     await cringe.delete()
 
 
@@ -63,6 +62,6 @@ CMD_HELP.update(
     {
         "waifu":
         ">`.wf`"
-        "\nUsage: Cringe anime stickers at your disposal."
+        "\nUsage: Random anime stickers at your disposal."
     }
 )
